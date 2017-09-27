0a619163afcc66148d9729b2595360fbd30d4e72
laihongyu <laihongyu@foxmail.com>
2017-09-27 15:32:39 +0800

    load init task before start

--- bootstrap.js
+++ bootstrap.js
@@ -1,16 +1,19 @@
 const taskManager = require('./lib/taskManager');
 
 module.exports = async()=> {
+    await taskManager._load();
+
     setTimeout(()=> {
         // taskManager.addTask({
         //     type: 'greeting',
         //     msg: 'hello worker'
         // });
-        taskManager.addTask({
-            type: 'babehub/list',
-            params: {
-                page: 1
-            }
-        });
+
+        // taskManager.addTask({
+        //     type: 'babehub/list',
+        //     params: {
+        //         page: 1
+        //     }
+        // });
     }, 3000);
 }
