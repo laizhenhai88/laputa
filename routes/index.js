02195d57f915ea613b8d48564cd173997721a7e7
laihongyu <laihongyu@foxmail.com>
2017-09-29 17:34:59 +0800

    add monitor and close limit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')();
 const tm = require('../lib/taskManager');
+const mongo = require('../lib/mongo');
 
 router.get('', async(ctx, next) => {
     ctx.body = 'Hello laputa!';
@@ -25,6 +26,17 @@ router.post('babehub', async(ctx, next)=> {
         }
     });
     ctx.body = {msg: 'success'}
+});
+
+router.get('monitor', async(ctx, next)=> {
+    await mongo.persist(async(client)=> {
+        ctx.body = {
+            task: await client.collection('task').count(),
+            waiting: await client.collection('task').count({status: 'init'}),
+            hb_list: await client.collection('hb_list').count(),
+            hb_detail: await client.collection('hb_detail').count(),
+        }
+    });
 });
 
 module.exports = router;
